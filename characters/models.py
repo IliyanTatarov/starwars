c516a19d01b6359bb47e5f59dcd216cfa955f6f0
Iliyan Tatarov <i.tataroff@gmail.com>
2021-05-19 07:09:15 +0300

    very basic collections frontend

--- characters/models.py
+++ characters/models.py
@@ -1,3 +1,5 @@
+import os
+
 from django.db import models
 from django.urls import reverse
 
@@ -6,8 +8,15 @@ class Collection(models.Model):
     csv_file = models.FileField(upload_to='csv/')
     created_at = models.DateTimeField(auto_now_add=True)
 
+    class Meta:
+        ordering = ['-id']
+
+    @property
+    def filename(self):
+        return os.path.basename(self.csv_file.name)
+
     def __str__(self):
-        return f'{self.csv_file.name}'
+        return f'{self.filename}'
 
     def get_absolute_url(self):
         return reverse('characters:collection', kwargs={
---
+++ static/js/app.js
@@ -0,0 +1,6 @@
+$.noConflict();
+jQuery( document ).ready(function($) {
+    $('#fetch_btn').click(function() {
+        $('#fetch_spinner').css('display', 'inline-block');
+    });
+});
